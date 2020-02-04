885d60bcdacdcbf3accd1ca2b9333f1f1a642095
Abigayle Franklin <Abifranklin@gmail.com>
2020-02-03 20:22:55 -0600

    Second build

---
+++ build/precache-manifest.182027332ed1288eeb17b202965e50e3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "91814f59f1738f8182e7771af210cbc0",
+    "url": "/index.html"
+  },
+  {
+    "revision": "0f4bcc9a4fae7562ca11",
+    "url": "/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "afac1bde877b32c24721",
+    "url": "/static/js/2.da10eb76.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.da10eb76.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0f4bcc9a4fae7562ca11",
+    "url": "/static/js/main.f143f3a7.chunk.js"
+  },
+  {
+    "revision": "cd9f3392618ed69d28ff",
+    "url": "/static/js/runtime-main.aaa190b3.js"
+  },
+  {
+    "revision": "3ab4645f19e787db2d53bcca10256e09",
+    "url": "/static/media/logo.3ab4645f.svg"
+  }
+]);
